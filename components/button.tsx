382b977238637616a57997b1f623baffbbf5aac4
Musaddek-Ahmed <musaddek59@gmail.com>
2024-06-28 04:01:58 +0600

    modified:   components/button.tsx 	modified:   components/footer.tsx 	modified:   components/getApp.tsx

--- components/button.tsx
+++ components/button.tsx
@@ -6,17 +6,18 @@ type ButtonProps ={
     type: 'button' | 'submit';
     title: string;
     icon?: string;
-    variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text';
+    variant: 'btn_dark_green' | 'btn_green' | 'btn_white_text' | 'btn_white' | 'btn_dark_green_outline';
+    full?: boolean;
 }
 
-const Button = ({type, title, icon, variant}: ButtonProps) => {
+const Button = ({type, title, icon, variant, full}: ButtonProps) => {
   return (
     <button
-        className={`flexCenter gap-3 rounded-full border ${variant}`}
-        type='button'
+        className={`flexCenter gap-3 rounded-full border ${variant} ${full && 'w-full'}`}
+        type={type}
     >
         {icon && <Image src={icon} alt='title' width={24} height={24} />}
-        <label className='bold-16 whitespace-nowrap'>{title}</label>
+        <label className='bold-16 whitespace-nowrap cursor-pointer'>{title}</label>
     </button>
   )
 }
